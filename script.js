0e3ea689b943486dd7c5861f006a3569722c9354
Dmitry Timanov <DTimanov@IBS.RU>
2022-01-07 17:47:10 +0000

    Add percentage symbol to slider text

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function compute() {
 //Update the rate value format
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
 
 //Check for positive values
